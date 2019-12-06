a216ea8db5654539bda4ea16b1b186982e99fd25
Oleksii Markhovskyi <olexiy.markhovsky@gmail.com>
2019-12-06 19:32:30 +0100

    fix dates parsing for mobile browsers

--- src/utils/date.js
+++ src/utils/date.js
@@ -1,3 +1,6 @@
 export function dateStringToTimestamp(dateString) {
-  return (new Date(dateString)).getTime();
+  // The "T" is getting inserted between "YYYY-MM-DD" and "HH:MM"
+  // to avoid "invalid date" errors in mobile browsers.
+  // @see: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/parse
+  return Date.parse(dateString.replace(' ', 'T'));
 }
