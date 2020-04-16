7116dcc716cec52dc9e4415dde8df87db86de933
Oleksii Markhovskyi <olexiy.markhovsky@gmail.com>
2020-04-16 10:44:22 +0300

    update deps

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,7 @@
 import babel from 'rollup-plugin-babel';
-import commonjs from 'rollup-plugin-commonjs';
+import commonjs from '@rollup/plugin-commonjs';
 import postcss from 'rollup-plugin-postcss';
-import resolve from 'rollup-plugin-node-resolve';
+import resolve from '@rollup/plugin-node-resolve';
 import {eslint} from 'rollup-plugin-eslint';
 import {uglify} from 'rollup-plugin-uglify';
 
